21ec94fcee39bb06f87fa930a15a948ab80fb013
Will Rossiter <will@silverstripe.com>
2009-07-16 04:03:52 +0000

    MINOR: updated googleadwords lang file

--- lang/en_US.php
+++ lang/en_US.php
@@ -2,43 +2,55 @@
 
 global $lang;
 
-$lang['en_US']['Adwords']['ALLADS'] = 'Viewing All Ads';
+$lang['en_US']['AdField']['PAUSE'] = 'Pause';
+$lang['en_US']['AdField']['SAVECHANGE'] = 'Save';
+$lang['en_US']['AdField']['UNPAUSE'] = 'Unpause';
 $lang['en_US']['Adwords']['ADSFORTHISPAGE'] = 'Viewing Ads For This Page';
+$lang['en_US']['Adwords']['ALLADS'] = 'Viewing All Ads';
+$lang['en_US']['Adwords']['LOGOUT'] = 'Sign out';
+$lang['en_US']['Adwords']['NOADSINVIEW'] = 'There are no ads in this view.';
 $lang['en_US']['Adwords']['VIEWADSFORTHISPAGE'] = 'View Ads for this page';
 $lang['en_US']['Adwords']['VIEWALLADS'] = 'View all ads';
-$lang['en_US']['Adwords']['NOADSINVIEW'] = 'There are no ads in this view.';
-$lang['en_US']['Adwords']['LOGOUT'] = 'Sign out';
+$lang['en_US']['Adwords.ss']['NOPERMISSION'] = 'You don\'t have permission to access this resource.';
+$lang['en_US']['AdwordsAccount']['PLURALNAME'] = array(
+	'Adwords Accounts',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['AdwordsAccount']['SINGULARNAME'] = array(
+	'Adwords Account',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['AdwordsAds']['PLURALNAME'] = array(
+	'Adwords Adss',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['AdwordsAds']['SINGULARNAME'] = array(
+	'Adwords Ads',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['AdwordsWords']['PLURALNAME'] = array(
+	'Adwords Wordss',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['AdwordsWords']['SINGULARNAME'] = array(
+	'Adwords Words',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['Captcha_Error.ss']['FAILEDLOGINATTEMPTS'] = 'Due to the number of failed login attempts, Google requires that you login with them before you can use this resource.';
+$lang['en_US']['Captcha_Error.ss']['LOGINTOSSADWORDS'] = 'Then login to the SilverStripe Adwords interface again after you have successfully verified your account with Google.';
+$lang['en_US']['Captcha_Error.ss']['VISITGOOGLE'] = 'Please visit <a href="https://www.google.com/accounts/DisplayUnlockCaptcha" target="_blank">https://www.google.com/accounts/DisplayUnlockCaptcha</a> to verify your account.';
 $lang['en_US']['CreationInterface']['LOADING'] = 'Loading...';
 $lang['en_US']['CreationInterface']['RETURNTOMAIN'] = 'Return To Main View';
-$lang['en_US']['AdField']['PAUSE'] = 'Pause';
-$lang['en_US']['AdField']['UNPAUSE'] = 'Unpause';
-$lang['en_US']['AdField']['SAVECHANGE'] = 'Save';
-$lang['en_US']['LoginField']['SIGNINWITH'] = 'Sign in with your';
 $lang['en_US']['LoginField']['ACCOUNT'] = 'Account';
 $lang['en_US']['LoginField']['EMAIL'] = 'Email:';
 $lang['en_US']['LoginField']['PASSWORD'] = 'Password:';
 $lang['en_US']['LoginField']['SIGNIN'] = 'Sign in';
-$lang['en_US']['Captcha_Error.ss']['FAILEDLOGINATTEMPTS'] = 'Due to the number of failed login attempts, Google requires that you login with them before you can use this resource.';
-$lang['en_US']['Captcha_Error.ss']['VISITGOOGLE'] = 'Please visit <a href="https://www.google.com/accounts/DisplayUnlockCaptcha" target="_blank">https://www.google.com/accounts/DisplayUnlockCaptcha</a> to verify your account.';
-$lang['en_US']['Captcha_Error.ss']['LOGINTOSSADWORDS'] = 'Then login to the SilverStripe Adwords interface again after you have successfully verified your account with Google.';
-$lang['en_US']['Ad_View.ss']['ADDNEWAD'] = 'Add a new ad to this page.';
-$lang['en_US']['KeywordField.ss']['KEYWORDS'] = 'Keywords';
-$lang['en_US']['KeywordField.ss']['EDIT'] = 'Edit';
-$lang['en_US']['KeywordField.ss']['SAVE'] = 'Save';
-$lang['en_US']['KeywordField.ss']['DELETE'] = 'Delete';
-$lang['en_US']['KeywordField.ss']['KEYWORD'] = 'Keyword';
-$lang['en_US']['KeywordField.ss']['BID'] = 'Bid';
-$lang['en_US']['KeywordField.ss']['URL'] = 'URL';
-$lang['en_US']['KeywordField.ss']['CLICKS'] = 'Clicks';
-$lang['en_US']['KeywordField.ss']['IMPRESSIONS'] = 'Impressions';
-$lang['en_US']['KeywordField.ss']['CTR'] = 'CTR';
-$lang['en_US']['KeywordField.ss']['POSITION'] = 'Position';
-$lang['en_US']['KeywordField.ss']['SCORE'] = 'Score';
-$lang['en_US']['KeywordField.ss']['ADD'] = 'Add:';
-$lang['en_US']['KeywordField.ss']['OPTIONALFIELDS'] = 'Note: The Bid and URL fields are optional, they will take revert to the defaults of the Ad if they are not set.';
-$lang['en_US']['Navigation.ss']['STATUS'] = 'Status';
-$lang['en_US']['Navigation.ss']['DEFAULTBID'] = 'Default Bid';
-$lang['en_US']['Navigation.ss']['IMPRESSIONS'] = 'Impressions';
-$lang['en_US']['Navigation.ss']['CTR'] = 'CTR';
+$lang['en_US']['LoginField']['SIGNINWITH'] = 'Sign in with your';
 
 ?>
